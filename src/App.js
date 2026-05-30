ac6e0d6048d7447fb609171d30ae00f1203925d2
shahazad08 <sk.shahazad@gmail.com>
2026-05-30 12:52:00 +0530

    Added: Event binding functionality added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import './App.css';
 
 function App() {
   // useState hook replaces constructor + this.state
+  const url = 'https://www.met.edu/';
   const [title] = useState('Hello from MET, and Welcome');
+
+   // onClick handler
+  const onClick = (event) => {
+    console.log("save button is clicked!", event);
+    window.open(url, "_blank");
+  };
+
+
   return (
     <div className="App">
       <h1>{title}</h1>
       <img src={logo}
+       onClick={onClick}
         alt="MET Logo"/>
     </div>
   );
