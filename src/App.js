ca17aa45ae4a5897b94d3143cfd254e36e5b82d7
shahazad08 <sk.shahazad@gmail.com>
2026-05-30 12:44:41 +0530

    Added: Data binding functionality added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import logo from './assets/img/Met_image.png';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // useState hook replaces constructor + this.state
+  const [title] = useState('Hello from MET, and Welcome');
   return (
     <div className="App">
-     <h1>Welcome to MET</h1>
+      <h1>{title}</h1>
+      <img src={logo}
+        alt="MET Logo"/>
     </div>
   );
 }
